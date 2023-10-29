b256459847bded8ae9b063acebeeee34a5770a42
Tiago Silveira Siqueira <tiagonomade@gmail.com>
2023-10-29 19:21:52 -0300

    Respostas.py

---
+++ Respostas.py
@@ -0,0 +1,147 @@
+**Resposta para o Projeto 1: Calculadora (calc.py)**
+```python
+def adicao(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b == 0:
+        return "Erro: Divisão por zero não é permitida."
+    return a / b
+```
+
+**Resposta para o Projeto 2: Lista de Tarefas (tarefas.py)**
+```python
+lista_de_tarefas = []
+
+def adicionar_tarefa(tarefa):
+    lista_de_tarefas.append(tarefa)
+
+def listar_tarefas():
+    for i, tarefa in enumerate(lista_de_tarefas, start=1):
+        print(f"{i}. {tarefa}")
+
+def marcar_como_concluida(indice):
+    if 1 <= indice <= len(lista_de_tarefas):
+        lista_de_tarefas[indice - 1] += " (Concluída)"
+
+def excluir_tarefa(indice):
+    if 1 <= indice <= len(lista_de_tarefas):
+        del lista_de_tarefas[indice - 1]
+```
+
+**Resposta para o Projeto 3: Conversor de Moeda (conversor.py)**
+```python
+import requests
+
+def converter_moeda(valor, moeda_de, moeda_para):
+    url = f"https://api.exchangerate-api.com/v4/latest/{moeda_de}"
+    response = requests.get(url)
+    data = response.json()
+    
+    taxa = data["rates"][moeda_para]
+    resultado = valor * taxa
+    return resultado
+```
+
+**Resposta para o Projeto 4: Jogo da Forca (forca.py)**
+```python
+import random
+
+palavras = ["python", "programacao", "desenvolvimento", "aprendizado", "tecnologia"]
+
+def escolher_palavra():
+    return random.choice(palavras)
+
+def jogar_forca():
+    palavra = escolher_palavra()
+    letras_certas = []
+    tentativas = 6
+    
+    while tentativas > 0:
+        letra = input("Digite uma letra: ").lower()
+        
+        if letra in palavra:
+            letras_certas.append(letra)
+        else:
+            tentativas -= 1
+        
+        palavra_escondida = "".join([letra if letra in letras_certas else "_" for letra in palavra])
+        print(palavra_escondida)
+        
+        if palavra_escondida == palavra:
+            print("Você ganhou! A palavra era:", palavra)
+            break
+    
+    if tentativas == 0:
+        print("Você perdeu. A palavra era:", palavra)
+
+**Resposta para o Projeto 5: Site Pessoal (site.py)**
+```python
+def criar_pagina_html():
+    with open('index.html', 'w') as file:
+        file.write('''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meu Site Pessoal</title>
+</head>
+<body>
+    <header>
+        <h1>Bem-vindo ao Meu Site Pessoal</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="#sobre">Sobre Mim</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contato">Contato</a></li>
+        </ul>
+    </nav>
+    <section id="sobre">
+        <h2>Sobre Mim</h2>
+        <p>Esta é a seção "Sobre Mim" do meu site pessoal.</p>
+    </section>
+    <section id="portfolio">
+        <h2>Portfolio</h2>
+        <p>Aqui, vou exibir meus projetos e trabalhos anteriores.</p>
+    </section>
+    <section id="contato">
+        <h2>Contato</h2>
+        <p>Você pode entrar em contato comigo pelo e-mail: meuemail@email.com</p>
+    </section>
+    <footer>
+        <p>&copy; 2023 Meu Site Pessoal</p>
+    </footer>
+</body>
+</html>
+        ''')
+
+# Exemplo de uso:
+criar_pagina_html()
+
+**Resposta para o Projeto 6: Análise de Dados (analise_dados.py)**
+```python
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Carregue o conjunto de dados (substitua 'data.csv' pelo seu arquivo de dados)
+dados = pd.read_csv('data.csv')
+
+# Explore os dados
+print(dados.head())  # Exibe as primeiras linhas dos dados
+
+# Realize análises estatísticas básicas
+estatisticas_descritivas = dados.describe()
+print(estatisticas_descritivas)
+
+# Crie visualizações
+plt.hist(dados['idade'], bins=10, color='blue', alpha=0.7)
+plt.title('Distribuição de Idade')
+plt.xlabel('Idade')
+plt.ylabel('Frequência')
+plt.show()
